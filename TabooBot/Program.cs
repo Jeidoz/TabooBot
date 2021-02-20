a217660e32effcec1bfd0be2a1976d28e85aeb16
Stanislav Khavruk <stas_khavruk@outlook.com>
2021-02-20 20:25:01 +0100

    Implemented dummy linux daemon for bot

--- TabooBot/Program.cs
+++ TabooBot/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
 
 namespace TabooBot
 {
-    class Program
+    internal class Program
     {
+        internal static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(Path.Combine("logs", "TabooBotLog.txt"), rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            try
+            {
+                CreateHostBuilder().Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Logger.Fatal($"{e.Message}\n{e.StackTrace}");
+                throw;
+            }
+            Log.CloseAndFlush();
+        }
+
+        private static IHostBuilder CreateHostBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                .UseSystemd()
+                .ConfigureServices(services =>
+                {
+                    services.AddLogging();
+                    services.AddSingleton<IHostedService, TabooChatBot>();
+                })
+                .ConfigureLogging(configLogging => configLogging.AddSerilog(dispose: true));
         }
     }
 }
---
+++ TabooBot/TabooChatBot.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace TabooBot
+{
+    public sealed class TabooChatBot : IHostedService
+    {
+        public static ITelegramBotClient BotClient;
+
+        public TabooChatBot()
+        {
+            InitializeBotClientInstance(Program.Configuration["BotToken"]);
+        }
+        private static void InitializeBotClientInstance(string botToken)
+        {
+            BotClient = new TelegramBotClient(botToken);
+        }
+
+        private void StartPolling(CancellationToken cancellationToken)
+        {
+            BotClient.StartReceiving(cancellationToken: cancellationToken);
+            Log.Logger.Information("Bot žačal číst vzkazy...");
+        }
+
+        private void StopPolling()
+        {
+            BotClient.StopReceiving();
+            Log.Logger.Information("Bot přestal pracovat...");
+            Log.CloseAndFlush();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    StartPolling(cancellationToken);
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        Thread.Sleep(TimeSpan.FromMinutes(1));
+                    }
+
+                    StopPolling();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("During bot work has been thrown error:");
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.Source);
+                    Console.WriteLine(ex.StackTrace);
+                    Console.WriteLine(ex.InnerException);
+                }
+            }, cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.Run(StopPolling, cancellationToken);
+        }
+    }
+}
