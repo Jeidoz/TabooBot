32b4c2676fd3eeede9c7a553fe61d813fc147fba
Stanislav Khavruk <stas_khavruk@outlook.com>
2021-02-20 21:16:23 +0100

    Add test "help" command

---
+++ TabooBot/Commands/HelpCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace TabooBot.Commands
+{
+    public sealed class HelpCommand : Command
+    {
+        public override string[] Triggers => new[] { "/help" };
+
+        public override async Task Execute(Message message)
+        {
+            const string response = "The list of available commands:\n" +
+                                    "/help – Show this message";
+            await TabooChatBot.BotClient.SendTextMessageAsync(
+                message.Chat.Id,
+                response,
+                replyToMessageId: message.MessageId);
+        }
+    }
+}
