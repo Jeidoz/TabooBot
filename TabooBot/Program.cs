7a9c96c94ecef18faef34321aba63271ea8434c3
Stanislav Khavruk <stas_khavruk@outlook.com>
2021-02-20 19:58:24 +0100

    Add project files.

---
+++ TabooBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TabooBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
